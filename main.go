7eda0731a95edc1b68e1a62c41244e4b13c1d054
Odhran Mahony <mahonyodhran@gmail.com>
2023-11-14 17:21:02 +0000

    added postgres db connection to Docker container - basic create table on run etc

--- main.go
+++ main.go
@@ -1,22 +1,49 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"log"
 	"net/http"
-	"time"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type Todo struct {
-	ID        int
 	Title     string
 	Notes     string
-	CreatedOn time.Time
-	UpdatedOn time.Time
 	Completed bool
 }
 
 func main() {
+	log.Println("App starting...")
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file:", err)
+	}
+
+	connStr := os.Getenv("DB_CONN")
+	db, err := sql.Open("postgres", connStr)
+
+	defer db.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Checking for Todo db...")
+
+	createTodoTable(db)
+
+	log.Println("App running...")
+
 	h1 := func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseFiles("index.html"))
 		tmpl.Execute(w, nil)
@@ -25,4 +52,21 @@ func main() {
 	http.HandleFunc("/", h1)
 
 	log.Fatal(http.ListenAndServe(":8008", nil))
+}
+
+func createTodoTable(db *sql.DB) {
+	query := `CREATE TABLE IF NOT EXISTS todo (
+		id SERIAL PRIMARY KEY,
+		title VARCHAR(50) NOT NULL,
+		notes VARCHAR(200) NOT NULL,
+		createdon TIMESTAMP DEFAULT NOW(),
+		updatedon TIMESTAMP DEFAULT NOW(),
+		completed BOOLEAN
+	)`
+
+	_, err := db.Exec(query)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
